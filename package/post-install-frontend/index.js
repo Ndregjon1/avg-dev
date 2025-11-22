e83f947e78e12592c63b92f18fab60913e876e02
111803 <ndregjonirenis@gmail.com>
2025-11-22 17:16:19 +0100

    Making PORT and URL configurable.

---
+++ package/post-install-frontend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || "https://raw.githubusercontent.com/111830/avg-dev/refs/heads/master/tools/script/post-install.sh" ;
+
+
+app.get("/", function(req, res){
+    return res.send(`wget -qO- ${URL} | bash`)
+});
+
+app.listen(PORT, function() {
+    console.log(`Server is listening on: ${PORT} `);
+});
+
